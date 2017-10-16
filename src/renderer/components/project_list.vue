80f9eeaad653955c18af192d2531e29a9604a305
ikitty <xkeitty@163.com>
2017-10-16 20:04:39 +0800

    update

--- src/renderer/components/project_list.vue
+++ src/renderer/components/project_list.vue
@@ -1,10 +1,12 @@
 <template>
 <div class="list">
     <div class="side">
-        <div class="side_hd">项目列表 &gt;&gt;</div>
-        <ul class="side_list">
-            <li v-for="(item, index) in projList" :key="index">&bull; {{item.name}}</li>
-        </ul>
+        <div class="side_hd">项目列表 <span>&gt;&gt;</span></div>
+        <div class="side_list_wrap">
+            <ul class="side_list">
+                <li v-for="(item, index) in projList" :key="index">&bull; {{item.name}}</li>
+            </ul>
+        </div>
     </div>
 
     <div class="main">
@@ -15,7 +17,7 @@
             <el-button type="" icon="setting">设置</el-button>
             <el-button type="danger" icon="delete2">删除</el-button>
         </div>
-        <el-input type="textarea" rows="16" resize="none" readonly="true" v-model="consoleInfo"> </el-input>
+        <el-input type="textarea" class="txts" rows=16 resize="none" readonly="true" v-model="consoleInfo"> </el-input>
     </div>
 </div>
 </template>
@@ -47,12 +49,13 @@
 <style scoped>
     .list {padding: 0px 0 ;height: 440px;;overflow: hidden;}
     .side {float:left;width: 200px;height: 100%;background: #f9f9f9;border-right: 1px solid #ddd;}
-    .side_hd {height: 42px;font: 700 14px/42px Simsun,Arial;padding-left:15px;color: #000; background: #E8E8E8;}
+    .side_hd {height: 40px;font-size:14px;font-weight:700;line-height:40px;padding-left:15px;color: #000; background: #E8E8E8;}
+    .side_hd span {font-family: simsun;}
+    .side_list_wrap {height:400px;overflow-y:scroll;}
     .side_list {padding-left: 15px;}
     .side_list li {height: 50px;line-height: 50px;border-bottom: 1px dotted #ddd;}
 
     .main {float:left;width: 570px;height: 100%;padding: 20px}
     .btns {padding-bottom: 20px;}
-    .main textarea {height: 350px;}
 
 </style>
